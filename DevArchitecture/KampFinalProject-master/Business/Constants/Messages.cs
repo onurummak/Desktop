6bf44bdecb0f53216af80d79661e0dd05db8c77e
onur ummak <oummak@ogr.iu.edu.tr>
2021-02-11 00:12:54 +0300

    11.02.2021

---
+++ DevArchitecture/KampFinalProject-master/Business/Constants/Messages.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages  //statik verdiğinde new yapmazsın sabittir
+    {                       // bu kısımda mesajların standardizasyonunu yaptık programın değişik yerlerinde fonksiyon gibi
+                            //programın değişik yerlerinde fonksiyon gibi kullanabiliriz
+        public static string ProductAdded = "Ürün Eklendi!"; //public değişkenler pascal case yazılır
+        public static string ProductNameInvalid = "Ürün İsmi Geçersiz!";
+        public static string MaintenanceTime="Sistem Bakımda";
+        public static object ProductListed="Ürünler Listelendi";
+
+        public static string ProductsListed { get; internal set; }
+    }
+}
---
+++ DevArchitecture/KampFinalProject-master/Core/Utilities/Results/IDataResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public interface IDataResult<T>:IResult  //T herşey olabilir ürün ürünadı vs. vs. herşey dönebilir
+    {  //interface interface implemente edebilir
+        T Data { get; }
+    }
+}
